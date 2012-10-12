a87dd593265002e5a19e0648a0e7de22e651e009
Kevin McCarthy <me@kevinmccarthy.org>
2012-10-11 15:56:20 -1000

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Batman',
-    version='0.3.2',
+    version='0.3.3',
     description='A deployment toolbelt',
     author='Kevin McCarthy',
     author_email='me@kevinmccarthy.org',
